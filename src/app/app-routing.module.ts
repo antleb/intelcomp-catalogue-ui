d5923e541ad17149e571d031f6b1c628da34a2c6
TheQuaker <cs2180008@di.uoa.gr>
2021-11-10 12:10:18 +0200

    minor fix in case of a single form group

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,9 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
-import {SearchComponent} from "../catalogue-ui/pages/search/search.component";
-import {LandingPageComponent} from "../catalogue-ui/pages/landingpages/dataset/landing-page.component";
 import {DynamicFormComponent} from "../catalogue-ui/pages/dynamic-form/dynamic-form.component";
 import {IntelcompSearchComponent} from "./pages/search/intelcomp-search.component";
 import {DatasetLandingPageComponent} from "./pages/landingpages/datasets/dataset-landing-page.component";
-import {FormBuilderComponent} from "../catalogue-ui/pages/form-builder/form-builder.component";
 
 const routes: Routes = [
   {
@@ -30,10 +27,6 @@ const routes: Routes = [
     path: 'form',
     component: DynamicFormComponent
     // loadChildren: () => import('../app/pages/dynamic-form/dynamic-form.module').then(m => m.DynamicFormModule)
-  },
-  {
-    path: 'formBuilder',
-    component: FormBuilderComponent
   }
 ];
 
