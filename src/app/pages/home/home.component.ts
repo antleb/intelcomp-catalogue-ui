39f9a198b59dc5d509dc55a6fa0845191d65ddb6
Stefania Martziou <stefania.martziou@imis.athena-innovation.gr>
2022-03-08 22:01:55 +0200

    Simple home page for the data catalogue

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
+import {SearchQuery} from "../../domain/search-query";
 
 @Component({
   selector: 'app-home',
@@ -6,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   title = 'catalogue-ui-ui';
+
+  searchForm: FormGroup;
+
+  constructor(public fb: FormBuilder, public router: Router) {
+    this.searchForm = fb.group({'query': ['']});
+  }
+
+  onSubmit(searchValue: SearchQuery) {
+    // console.log('searchValue =>', searchValue);
+    if(searchValue.query=='')
+      return this.router.navigate(['/search']);
+    return this.router.navigate(['/search', searchValue]);
+  }
 }
