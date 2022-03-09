e4aed705bf7d88a42c566403b0c7885559a76b83
Stefania Martziou <stefania.martziou@imis.athena-innovation.gr>
2022-03-09 11:42:40 +0200

    Temporary commit for the search page

--- src/app/pages/search/intelcomp-search.component.ts
+++ src/app/pages/search/intelcomp-search.component.ts
@@ -4,7 +4,8 @@ import {SearchService} from "../../../catalogue-ui/services/search.service";
 
 @Component({
   selector: 'app-intelcomp-search',
-  templateUrl: '../../../catalogue-ui/pages/search/search.component.html',
+  // templateUrl: '../../../catalogue-ui/pages/search/search.component.html',
+  templateUrl: './intelcomp-search.component.html',
   providers: [SearchService]
 })
 
